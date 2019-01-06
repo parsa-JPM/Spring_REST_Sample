d37456dec220d4d9c6b3fc3ddd09df8fa1d90832
parsa mihandoost <mihandoost.p@gmail.com>
2019-01-06 17:26:59 +0330

    refactor style

--- src/test/java/ir/codefather/REST/TestSpringRestExample.java
+++ src/test/java/ir/codefather/REST/TestSpringRestExample.java
@@ -11,7 +11,7 @@ public class TestSpringRestExample {
 
     public static final String SERVER_URI = "http://localhost:8080/";
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         testGetDummyEmployee();
         System.out.println("*****");
@@ -31,34 +31,36 @@ public class TestSpringRestExample {
         RestTemplate restTemplate = new RestTemplate();
         //we can't get List<Employee> because JSON convertor doesn't know the type of
         //object in the list and hence convert it to default JSON object type LinkedHashMap
-        List<LinkedHashMap> emps = restTemplate.getForObject(SERVER_URI+EmpRestURIConstants.GET_ALL_EMP, List.class);
+        List<LinkedHashMap> emps = restTemplate.getForObject(SERVER_URI + EmpRestURIConstants.GET_ALL_EMP, List.class);
 
-        for(LinkedHashMap map : emps){
-            System.out.println("ID="+map.get("id")+",Name="+map.get("name")+",CreatedDate="+map.get("createdDate"));;
+        for (LinkedHashMap map : emps) {
+            System.out.println("ID=" + map.get("id") + ",Name=" + map.get("name") + ",CreatedDate=" + map.get("createdDate"));
+            ;
         }
     }
 
     private static void testCreateEmployee() {
         RestTemplate restTemplate = new RestTemplate();
         Employee emp = new Employee();
-        emp.setId(1);emp.setName("Pankaj Kumar");
-        Employee response = restTemplate.postForObject(SERVER_URI+EmpRestURIConstants.CREATE_EMP, emp, Employee.class);
+        emp.setId(1);
+        emp.setName("Pankaj Kumar");
+        Employee response = restTemplate.postForObject(SERVER_URI + EmpRestURIConstants.CREATE_EMP, emp, Employee.class);
         printEmpData(response);
     }
 
     private static void testGetEmployee() {
         RestTemplate restTemplate = new RestTemplate();
-        Employee emp = restTemplate.getForObject(SERVER_URI+"/rest/emp/1", Employee.class);
+        Employee emp = restTemplate.getForObject(SERVER_URI + "/rest/emp/1", Employee.class);
         printEmpData(emp);
     }
 
     private static void testGetDummyEmployee() {
         RestTemplate restTemplate = new RestTemplate();
-        Employee emp = restTemplate.getForObject(SERVER_URI+EmpRestURIConstants.DUMMY_EMP, Employee.class);
+        Employee emp = restTemplate.getForObject(SERVER_URI + EmpRestURIConstants.DUMMY_EMP, Employee.class);
         printEmpData(emp);
     }
 
-    public static void printEmpData(Employee emp){
-        System.out.println("ID="+emp.getId()+",Name="+emp.getName()+",CreatedDate="+emp.getCreatedDate());
+    public static void printEmpData(Employee emp) {
+        System.out.println("ID=" + emp.getId() + ",Name=" + emp.getName() + ",CreatedDate=" + emp.getCreatedDate());
     }
 }
